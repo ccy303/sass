15d8985e6afa8c240594f297977ad147071dd18b
unknown <303752792@qq.com>
2024-07-07 18:05:17 +0800

    init

--- src/http/user.js
+++ src/http/user.js
@@ -4,18 +4,12 @@ export const getUserInfo = () => {
     return request.get({ url: `${import.meta.env.VITE_BASE_URL}/minapp/wechat/user/getUserInfo` }, false);
 };
 
-export const login = token => {
-    return request.post({
-        url: `${import.meta.env.VITE_BASE_URL}/auth/login`,
-        data: {
-            tenantId: import.meta.env.VITE_TENANT_ID,
-            appId: import.meta.env.VITE_APP_ID,
-            clientId: import.meta.env.VITE_CLIENT_ID,
-            grantType: "xcx",
-            xcxCode: token
-        },
-        headers: { isEncrypt: true }
-    });
+export const login = code => {
+    return request.get({ url: `${import.meta.env.VITE_BASE_URL}/blade-system/user/getToken`, data: { code } });
+};
+
+export const beTenant = data => {
+    return request.get({ url: `${import.meta.env.VITE_BASE_URL}/blade-system/tenant/activate`, data });
 };
 
 export const bingPhone = data => {
@@ -30,48 +24,4 @@ export const editUserInfo = data => {
         url: `${import.meta.env.VITE_BASE_URL}/minapp/wechat/user/editUserInfo`,
         data
     });
-};
-
-// 分页查询用户收藏藏品列表
-export const getUserFavorCollection = data => {
-    return request.get({ url: `${import.meta.env.VITE_BASE_URL}/minapp/my/fav/relic/pageList`, data });
-};
-
-// 分页查询用户收藏博物馆列表
-export const getUserFavorMuseum = data => {
-    return request.get({ url: `${import.meta.env.VITE_BASE_URL}/minapp/my/fav/museum/pageList`, data });
-};
-
-// 新增小程序用户收藏记录
-export const addUserFavor = data => {
-    return request.post({ url: `${import.meta.env.VITE_BASE_URL}/minapp/my/fav/add`, data });
-};
-
-// 删除小程序用户收藏记录
-export const delUserFavor = data => {
-    return request.post({ url: `${import.meta.env.VITE_BASE_URL}/minapp/my/fav/delete`, data });
-};
-
-// 检查收藏
-export const checkUserFavor = data => {
-    return request.post({ url: `${import.meta.env.VITE_BASE_URL}/minapp/my/fav/check`, data }, false);
-};
-
-// 分页查询用户 场馆预约 信息列表
-export const getVenueAppointmentList = data => {
-    return request.get({ url: `${import.meta.env.VITE_BASE_URL}/minapp/mpUserReservation/museum/pageList`, data });
-};
-
-//分页查询用户 文博活动预约 信息列表
-export const getActivityAppointmentList = data => {
-    return request.get({ url: `${import.meta.env.VITE_BASE_URL}/minapp/mpUserReservation/optActivity/pageList`, data });
-};
-
-// 获取用户预约信息详细信息
-export const getAppointmentDetail = id => {
-    return request.get({ url: `${import.meta.env.VITE_BASE_URL}/minapp/mpUserReservation/${id}` });
-};
-
-export const cancelAppointment = id => {
-    return request.put({ url: `${import.meta.env.VITE_BASE_URL}/minapp/mpUserReservation/cancelReservation/${id}` });
 };
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,6 +1,41 @@
 <template>
-    <div>12</div>
+    <base-page :widthNav="false" padding="0">
+        <base-image :size="[400, '100%']" :src="bg"></base-image>
+
+        <div class="w-95% mx-auto mt--20 bg-#fff z-99 relative p-20 box-border border-rd-10 flex items-center">
+            <base-avatar :size="130" :src="avatar"></base-avatar>
+            <div class="ml-20">
+                <base-text :size="30" :bold="true">Hi~微信用户</base-text>
+            </div>
+        </div>
+
+        <div class="w-95% mx-auto bg_#fff mt-20">
+            <base-card label="我的功能">
+                <base-grid :column="4" :border="false">
+                    <base-grid-item>
+                        <div class="flex flex-col items-center">
+                            <base-image :size="100" :src="gdfw" />
+                            <base-text>我的摊位</base-text>
+                        </div>
+                    </base-grid-item>
+                    <base-grid-item>
+                        <div class="flex flex-col items-center">
+                            <base-image :size="100" :src="wddd" />
+                            <base-text>我的订单</base-text>
+                        </div>
+                    </base-grid-item>
+                </base-grid>
+            </base-card>
+        </div>
+    </base-page>
 </template>
 
-<script setup></script>
+<script setup>
+    import bg from "@/images/9a82219bedcae5c2.jpeg";
+    import avatar from "@/images/mine/default.png";
+    import gdfw from "@/images/mine/gdfw.png";
+    import wddd from "@/images/mine/wddd.png";
+
+    const bgs = ref([{ url: bg }]);
+</script>
 <style lang="scss" scoped></style>
